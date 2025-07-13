e10a57392cc54b47f0b2b45df49e8c08bbe2e01e
mohamedemad121 <mohamedemmad0@gmail.com>
2025-07-13 22:22:53 +0200

    feat: Update app routing to include PaymentView; refactor SignInProcess and restore StackSignProcess component

--- lib/core/constant/app_router.dart
+++ lib/core/constant/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:foodninja/presentation/on_boarding/views/page_view.dart';
 import 'package:foodninja/presentation/on_boarding/widgets/on_boarding2.dart';
+import 'package:foodninja/presentation/paymentview/views/payment_view.dart';
 import 'package:foodninja/presentation/sign_in/views/sign_in_view.dart';
 import 'package:foodninja/presentation/sign_up/view/sign_up.dart';
 import 'package:foodninja/presentation/sing_in_process/views/sign_in_process.dart';
@@ -13,6 +14,7 @@ class AppRouter {
   static const ksignup = '/signup';
   static const ksignin = '/signin';
   static const ksigninprocess = '/signinprocess';
+  static const kpaymentview = '/paymentview';
   static final router = GoRouter(
     routes: [
       GoRoute(path: '/', builder: (context, state) => SplashView()),
@@ -20,7 +22,11 @@ class AppRouter {
       GoRoute(path: kpageviewbody, builder: (context, state) => PageViewBody()),
       GoRoute(path: ksignup, builder: (context, state) => SignUp()),
       GoRoute(path: ksignin, builder: (context, state) => SignInView()),
-      GoRoute(path: ksigninprocess, builder: (context, state) => SignInProcess()),
+      GoRoute(
+        path: ksigninprocess,
+        builder: (context, state) => SignInProcess(),
+      ),
+      GoRoute(path: kpaymentview, builder: (context, state) => PaymentView()),
     ],
   );
 }
--- lib/presentation/sing_in_process/views/sign_in_process.dart
+++ lib/presentation/sing_in_process/views/sign_in_process.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:foodninja/core/components/custom_button.dart';
 import 'package:foodninja/core/components/custom_text_field.dart';
-import 'package:foodninja/presentation/sing_in_process/widgets/stack_sign_process.dart';
+import 'package:foodninja/core/components/stack_sign_process.dart';
+import 'package:foodninja/core/constant/app_router.dart';
+import 'package:go_router/go_router.dart';
 
 class SignInProcess extends StatelessWidget {
   const SignInProcess({super.key});
@@ -10,21 +12,8 @@ class SignInProcess extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          StackSignProcess(),
-          SizedBox(height: 19),
-          Padding(
-            padding: const EdgeInsets.only(left: 25),
-            child: Text(
-              'This data will be displayed in your account\nprofile for security',
-              style: TextStyle(
-                fontSize: 12,
-                fontFamily: 'BentonSansBook',
-                color: Colors.black,
-              ),
-            ),
-          ),
+          StackSignProcess(top: 188),
           SizedBox(height: 20),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 14),
@@ -41,7 +30,11 @@ class SignInProcess extends StatelessWidget {
             child: CustomTextField(hintText: ' Mobile Number'),
           ),
           SizedBox(height: 220),
-          Center(child: CustomButton(text: 'Next')),
+          Center(child: InkWell(
+            onTap: () {
+              context.push(AppRouter.kpaymentview);
+            },
+            child: CustomButton(text: 'Next'))),
         ],
       ),
     );
